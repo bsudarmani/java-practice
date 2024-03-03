c1463d7a5d56a38ce6da2c20cf1f29b765fd1bc3
Sudarmani B <bsudarmani2006@gmail.com>
2024-03-03 04:24:56 +0000

    sort element

--- Basics/2/SortElement.java
+++ Basics/2/SortElement.java
@@ -7,30 +7,58 @@ class SortElement
      Scanner sc=new Scanner(System.in);
      System.out.println("Enter the number in array");
      int num=sc.nextInt();
-      int number[]=new int[num];
-     int arr1[]=new int[num];
-     int arr2[]=new int[num];
+     int arr1[]=new int[100];
+     int arr2[]=new int[100];
      int odd=0,even=0;
       System.out.println("enter the element in one by one");
      for(int i=0;i<num;i++)
      {
-        number[i]=sc.nextInt();
-     }
-     for(int i=0;i<num;i++)
-     {
-        if(number[i]%2!=0)
+        if(i%2!=0)
          {
-            arr1[even++]=number[i];
+            arr1[even]=sc.nextInt();
+            even++;
          }
          else
          {
-            arr2[odd++]=number[i];
+            arr2[odd]=sc.nextInt();
+            odd++;
          }
      }
-     for(int i=0;i<num;i++)
+     for(int i=0;i<odd-1;i++)
      {
-     System.out.println(arr1[i]);
-     System.out.println(arr2[i]);
+       for(int j=i+1;j<odd;j++)
+       {
+         if(arr2[i]<arr2[j])
+         {
+            int temp=arr2[i];
+            arr2[i]=arr2[j];
+            arr2[j]=temp;
+         }
+       }
+     }
+     for(int i=0;i<even-1;i++)
+     {
+      for(int j=i+1;j<even;j++)
+      {
+       if(arr1[i]>arr1[j])
+       {
+          int temp=arr1[i];
+          arr1[i]=arr1[j];
+          arr1[j]=temp;
+       }
+      }
+     }
+     int len=odd>even?odd:even;
+     for(int i=0;i<len;i++)
+     {
+      if(i<odd)
+      {
+         System.out.print(" "+arr2[i]);
+      }
+      if(i<even)
+      {
+         System.out.print(" "+arr1[i]);
+      }
      }
     }
 }
